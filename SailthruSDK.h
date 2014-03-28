afe3b13700236cfd6d86fdf270d52e14ea3a89d3
David Hoerl <dhoerl@sailthru.com>
2014-03-28 17:41:34 -0400

    Need files to test

---
+++ SailthruSDK.h
@@ -0,0 +1,15 @@
+//
+//  SailthruSDK.h
+//  libSailthru
+//
+//  Copyright (c) 2013-2014 Sailthru, Inc. All rights reserved.
+//
+
+// Primary interface to libSailthru.a
+#import "STManager.h"
+
+// Instantiate these to post activity to App Tracker
+#import "STEvent.h"
+
+// Set log level threshhold
+#import "STLog.h"
